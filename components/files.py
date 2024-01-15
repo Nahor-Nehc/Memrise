c28b4eb6b61005170e4a58dbbab08935b5db75d4
0uranos <rohanchen00@gmail.com>
2024-01-15 07:33:25 +0000

    loaded verses

--- components/files.py
+++ components/files.py
@@ -1,5 +1,6 @@
 from components.constants import BOOKS
 import os
+import re
 
 class Verse:
   def __init__(self, number, text = ""):
@@ -11,33 +12,40 @@ class Verse:
   
   def get_text(self):
     return self.text
+  
+  def __repr__(self):
+    return f"<Verse {str(self.number)}: {self.text}>"
 
 class Chapter:
-  def __init__(self, number, verses: list[Verse] = []):
+  def __init__(self, number, verses: dict[int, Verse] = {}):
     self.number = number
     self.verses = verses
   
-  def set_verses(self, verses: list[Verse] = []):
+  def set_verses(self, verses: dict[int, Verse] = {}):
     self.verses = verses
   
   def get_verses(self):
     return self.verses
-
-  def append(self, verse: Verse):
-    self.verses.append(verse)
   
-  def 
+  def add_verse(self, number, verse:Verse):
+    self.verses[number] = verse
+  
+  def __repr__(self):
+    return f"<Chapter {self.number} with {len(self.verses)} verses>"
 
 class Book:
-  def __init__(self, name, chapters: list[Chapter] = []):
+  def __init__(self, name, chapters: dict[int, Chapter] = {}):
     self.name = name
     self.chapters = chapters
   
-  def set_chapters(self, chapters: list[Chapter] = []):
+  def set_chapters(self, chapters:  dict[int, Chapter] = {}):
     self.chapters = chapters
   
   def get_chapters(self):
     return self.chapters
+  
+  def add_chapter(self, number, chapter:Chapter):
+    self.chapters[number] = chapter
 
 def get_filename(book_index):
   return BOOKS[book_index] + ".txt"
@@ -45,16 +53,34 @@ def get_filename(book_index):
 def get_book_contents(filename):
   
   with open(os.path.join("data", filename), "r") as f:
-    contents = f.read().strip().split("\n")
+    contents = f.read().strip().split("\n")[1:]
   
   return contents
 
 def process_contents(contents) -> Book:
-  chapters = Chapters()
+  chapters = {}
+  
+  current_chapter = 1
+  chapter = Chapter(current_chapter)
   for verse in contents:
-    pass
+    verse_number = re.search(':(.*)]', verse).group(1) # type:ignore
+    new_verse = Verse(verse_number, "]".join(verse.split("]")[1:]))
+    
+    #print(verse.split(":")[0][1:])
+    if int(verse.split(":")[0][1:]) != current_chapter:
+      chapters[current_chapter] = chapter
+      current_chapter += 1
+      chapter = Chapter(current_chapter, verses = {})
+    
+    chapter.add_verse(verse_number, new_verse)
+    
+  chapters[current_chapter] = chapter
+  
+  return Book(chapters)
 
 def process(book_index):
   filename = get_filename(book_index)
   contents = get_book_contents(filename)
   contents = process_contents(contents)
+
+process(BOOKS.index("1 John"))
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 from components.find import *
 from components.files import *
 
-print(rand_book("Romans", "Genesis"))
-print(rand_book())
-print("======")
+# print(rand_book("Romans", "Genesis"))
+# print(rand_book())
+# print("======")
 
-print(get_book_contents(0))
+# print(get_book_contents(0))
 
 # print(rand_chapter("MAT", "MRK"))
 # print()
